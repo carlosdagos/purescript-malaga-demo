bfad6787ab15ee8cc3529966fdbf9204ad930215
Carlos D <carlos.dagostino@gmail.com>
2016-12-16 00:32:10 +0100

    Add infix example

--- example-app/src/Examples/List.purs
+++ example-app/src/Examples/List.purs
@@ -5,8 +5,14 @@ import Data.Maybe
 
 data List a = End | Cons a (List a)
 
+infixr 6 Cons as :
+
 myList :: List Int
 myList = Cons 1 (Cons 2 (Cons 3 End))
+
+-- It's equivalent, only showing an infix contructor
+myInfixList :: List Int
+myInfixList = 1 : 2 : 3 : End
 
 emptyList :: List Int
 emptyList = End
@@ -18,3 +24,10 @@ find x (Cons y rest) =
        else find x rest
 find _ End = Nothing
 
+-- It's equivalent, only showing a pattern-match with an infix constructor
+findWithInfixPattern :: Int -> List Int -> Maybe Int
+findWithInfixPattern x (y:rest) =
+    if x == y
+       then Just x
+       else findWithInfixPattern x rest
+findWithInfixPattern _ End = Nothing
